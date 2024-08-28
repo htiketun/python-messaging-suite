8c7eb205ba785f7d72d4e4a16804493338f6ebb7
htiketun <kohtiketun@outlook.com>
2024-08-28 12:18:42 +0000

    Improve config validation

--- telegram_sync/config.py
+++ telegram_sync/config.py
@@ -26,3 +26,4 @@ MEDIA_BASE_PATH = os.getenv("MEDIA_BASE_PATH", "media/")# Commit 4: 2024-07-08T0
 # Commit 19: 2024-08-12T03:35:41
 # Commit 2: 2024-07-03T08:43:22
 # Commit 19: 2024-08-12T03:38:42
+# Commit 26: 2024-08-28T12:18:42
