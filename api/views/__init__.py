477308f1ecee1fa65ccaee522bb9725364661ee0
htiketun <kohtiketun@outlook.com>
2024-11-06 16:57:04 +0000

    Add new view component

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -4,4 +4,4 @@ from .token import CustomTokenObtainPairView
 from .logout import LogoutView
 from .profile import ProfileView, UpdateProfileView, ChangePasswordView
 from .general import SyncSavedMessages, GetSyncedSavedMessages, SyncToDoList, GetSyncedToDoList
-from .csrf_cookie import csrf_cookie
+from .csrf_cookie import csrf_cookie# Commit 56: 2024-11-06T16:57:04
