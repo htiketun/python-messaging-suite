d659b550b1ce5c245d288803c5ac5d8dfad84a08
htiketun <kohtiketun@outlook.com>
2024-12-02 11:29:36 +0000

    Add new view component

--- api/views/hello.py
+++ api/views/hello.py
@@ -3,4 +3,4 @@ from rest_framework.response import Response
 
 class HelloWorld(APIView):
     def get(self, request):
-        return Response({"message": "Hello, World!"})
+        return Response({"message": "Hello, World!"})# Commit 67: 2024-12-02T11:29:36
