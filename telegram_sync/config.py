2750ba69f7bc355322ee159534a87cc2f88736cf
htiketun <kohtiketun@outlook.com>
2025-03-24 18:43:42 +0000

    Refactor config structure; Refactor config structure; Improve config validation; Add new environment variable; Add new environment variable; Improve config validation; Add new environment variable; Update view layout

--- telegram_sync/config.py
+++ telegram_sync/config.py
@@ -15,3 +15,4 @@ MEDIA_BASE_PATH = os.getenv("MEDIA_BASE_PATH", "media/")# Commit 4: 2024-07-08T0
 # Commit 86: 2025-01-15T21:43:16
 # Commit 94: 2025-02-03T14:58:27
 # Commit 101: 2025-02-20T00:34:32
+# Commit 115: 2025-03-24T18:43:42
