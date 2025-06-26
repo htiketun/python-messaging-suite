6290844935beb8f7432aaac532ef4e29ddba21a4
htiketun <kohtiketun@outlook.com>
2025-06-26 08:40:26 +0000

    Update view layout

--- api/views/token.py
+++ api/views/token.py
@@ -2,4 +2,4 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny
 
 class CustomTokenObtainPairView(TokenObtainPairView):
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny]# Commit 155: 2025-06-26T08:40:26
